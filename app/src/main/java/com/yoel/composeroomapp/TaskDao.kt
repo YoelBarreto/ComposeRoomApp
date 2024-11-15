0e212e512cea25a0dc9f7700996b443abc4b88ce
Yoel <yobelb8@gmail.com>
2024-11-15 11:42:56 +0000

    Pequeños cambios

--- app/src/main/java/com/yoel/composeroomapp/TaskDao.kt
+++ app/src/main/java/com/yoel/composeroomapp/TaskDao.kt
@@ -4,9 +4,10 @@ import androidx.room.*
 
 @Dao
 interface TaskDao {
-    @Insert suspend fun insert(task: Task)
-    @Query("SELECT * FROM tasks")
+    @Insert
+    suspend fun insert(task: Task)
+        @Query("SELECT * FROM tasks")
     suspend fun getAllTasks(): List<Task>
-    @Update suspend fun update(task: Task)
-    @Delete suspend fun delete(task: Task)
+        @Update suspend fun update(task: Task)
+        @Delete suspend fun delete(task: Task)
 }
